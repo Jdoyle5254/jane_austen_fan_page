b5de36ea12a5a99a84b5b1be08096eb9508d1701
Jen Doyle <jdoyle5254@users.noreply.github.com>
2022-03-03 21:30:01 -0600

    initial repo and set up data file

---
+++ src/data/bookData.js
@@ -0,0 +1,15 @@
+const prideChars = ["Lizzie", "Darcy", "Mr. Bingley", "Jane", "Mr.Wickham"];
+const senseChars = ["Elinor", "Edward Ferras", "Colonel Brandon", "Willoughby"];
+const mansChars = ["Fanny Price", "Edmund", "Maria Bertram", "Henry Crawford", "Mrs. Norris"];
+const emmaChars = ["Emma", "George Knightly", "Frank Churchill", "Harriet Smith" ];
+const persChars = ["Anne", "Captain Wentworth", "Lady Russell", "William Elliot"]
+const northChars = ["Catherine Morland", "Henry Tilney", "Isabella Thorpe", "Eleanor Tilney"]
+
+export const bookData =[
+{id: 1, title: "Pride and Prejudice", publishDate: "1813", chapters: "61",  characters: prideChars},
+{id: 2, title: "Sense and Sensibility", publishDate: "1811", chapters: "50", characters: senseChars},
+{id: 3, title: "Mansfield Park", publishDate: "1814", chapters:"48", characters: mansChars},
+{id: 4, title:"Emma", publishDate: "1815", chapters:"55", characters: emmaChars},
+{id: 5, title:"Persuasion", publishDate: "1817", chapters:"24", characters: persChars},
+{id: 6, title:"Northanger Abbey", publishDate: "1817", chapters:"31", characters: northChars},
+]
